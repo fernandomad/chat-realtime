64b0fdb531b53c18a1367f32a883f74ea3d74efc
fertaf <fer.raf.mad.tafolla@outlook.com>
2016-01-31 18:19:51 -0600

    intento crear seccion de usuarios

--- index.js
+++ index.js
@@ -10,21 +10,17 @@ var cookieSession = require('cookie-session')
 
 app.use(cookieSession({
   name: 'session',
-  keys: ['key1', 'key2']
+  keys: ['key1', 'key2', 'key22']
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: 'abcdef', cookie: { maxAge: 60000 }}))
-
-//app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req, res){
   if(req.session.logi==true)
-    { res.redirect('/menu');
-      //res.render('a',{u:req.session.user});
-    }
+    { res.redirect('/menu');}
   else
     {res.render('login');}
   
@@ -35,68 +31,129 @@ res.redirect('/');
 
   });
 app.post('/menu',function(req,res){
-
-  
-  
     req.session.logi=true;
-    req.session.ide=req.body.password;
+    req.session.password=req.body.password;
     req.session.user=req.body.usuario;
     res.redirect('/menu');
-  
-
-  
 });
 app.get('/menu',function(req,res){
   if(req.session.logi==true){
-  res.render('a',{u:req.session.user});}
+    res.render('a',{u:req.session.user});
+    }
   else
     {res.send('err');}
 });
-app.get('/ass',function(req,res){
+
+app.get('/as',function(req,res){
+  console.log(app_user2);
+  });
+/*app.get('/ass',function(req,res){
+  console.log('-------------------');
+  console.log(app_user);
+  console.log('-------------------');
 for(var socketid in app_user){
+var con=0;
          console.log(app_user[socketid]);
-       }
+         for(var socketid2 in app_user){
+          if(app_user[socketid2].user===app_user[socketid].user)
+            {
+              con++;
+            }
+          if( con === 1)//{console.log('se encontro 1 vece'+app_user[socketid2].user)}
+            if( con === 2){//console.log('se encontro 2 veces'+app_user[socketid2].user);
+          app_user2[app_user[socketid2].ide]=app_user[socketid2];}
+              //else{console.log('se encontro mas de dos veces'+app_user[socketid2].user)}
+          
+         //console.log(app_user[socketid2]);
+
+        }
+    }
+    console.log('-------------------');
+  console.log(app_user2);
+  console.log('-------------------');
        res.send("ok")
-});
+});*/
+/*function us(){
+for(var socketid in app_user){
+var con=0;
+         //console.log(app_user[socketid]);
+         for(var socketid2 in app_user){
+          if(app_user[socketid2].user===app_user[socketid].user)
+            {
+              con++;
+            }
+            if( con === 1){
+              console.log('agregoa2');
+          app_user2[app_user[socketid2].ide]=app_user[socketid2];}
+        }
+    }
+}*/
 
-
+var app_user2={};
 var app_user={};
 io.on('connection', function(socket){
 	console.log('a user connected');
+  
 	io.emit('chat message',''+socket.id);
     	//console.log(socket.id);
 	var exten='/#';
   	socket.on('disconnect', function(){
       var idee=socket.id.replace(exten,'');
-      console.log('-----------------');
-      console.log(app_user[idee]);
        delete app_user[idee];
-       console.log('-----------------');
-      console.log(app_user[socket.id]);
-    	console.log('user disconnected');
-    	
+       delete app_user2[idee];
+       delete app_user2['ok'];
+       console.log('disconnect');
   	});
   	socket.on('chat message', function(msg){
-      
-       
-
+      if(msg.des=='todos'){
+        io.emit('chat message', msg.msj);
+      }else{
       for(var socketid in app_user){
-         //console.log(app_user[socketid].ide);
-        
-        if(app_user[socketid].user=='ref'){
-         // console.log('entro');
-          io.to('/#'+app_user[socketid].ide).emit('chat message',msg);
+        if(app_user[socketid].user==msg.des){
+          io.to('/#'+app_user[socketid].ide).emit('chat message',msg.msj);
+          if(app_user[socketid].ide!=msg.ide){
+          io.to('/#'+msg.ide).emit('chat message',msg.msj);}
         }
+      }
       }
     	//io.emit('chat message', msg);
   	});
 
     socket.on('acceso',function(user){
-      
+    
       
       app_user[user.ide]=user;
 
+      for(var socketid1 in app_user2){
+        if(app_user2[socketid1].ide){
+      console.log(socket.id);
+      io.to(socket.id).emit('agrega', app_user2[socketid1].user);
+      }
+      }
+      if (app_user2.ok===undefined) {
+        app_user2.ok='ok';
+      
 
+      app_user2[user.ide]=app_user[user.ide];
+      }else{
+        var i=false;
+        for (var socketid2 in app_user2) {
+            if(app_user[user.ide].user==app_user2[socketid2].user)
+              {i=true;} 
+        }
+        if(!i){{app_user2[user.ide]=app_user[user.ide];}}
+      }
+      
+     // console.log(user.no);
+      //if(user.no){
+      //  user.no=false;
+      
+     // }else{
+      for(var socketid in app_user2){
+        if(app_user2[socketid].user!==user.user){io.emit('agrega', user.user);}
+      }
+      //}
+      session.ide=user.ide;
     });
 
     
